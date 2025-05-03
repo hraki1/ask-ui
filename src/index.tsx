c5ff3c10230b6a394297a414990393d21daccd3e
ahmad alhraki <ahmadhraki752@gmail.com>
2025-05-03 21:00:45 +0300

    Add SpeedInsights from vercal

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,16 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <>
+    <Provider store={store}>
+      <App />
+    </Provider>
+    <SpeedInsights />
+  </>
 );
